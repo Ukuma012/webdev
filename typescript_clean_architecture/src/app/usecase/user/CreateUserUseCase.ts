18b0011f751597f54f509eb1e5a938a53251cd48
Ukuma012 <Ukuma012@gmail.com>
2023-08-08 14:52:40 +0900

    use case

---
+++ typescript_clean_architecture/src/app/usecase/user/CreateUserUseCase.ts
@@ -0,0 +1,16 @@
+import { User } from "../../../domain/User";
+
+class CreateUserUseCase {
+    private userRepository: IUserRepository;
+
+    public constructor(userRepository: IUserRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public createUser(user: User): Promise<User> {
+        const userDTO = toCreateUserDTO(user);
+        return this.userRepository.create(userDTO);
+    }
+}
+
+export { CreateUserUseCase };
