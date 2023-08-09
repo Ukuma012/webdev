90bfaaeaca88ce5c4c80e40bfa5285402201cb6c
Ukuma012 <Ukuma012@gmail.com>
2023-08-09 14:27:13 +0900

    repository

--- typescript_clean_architecture/src/interface/database/MySQL/UserRepositoryImpl.ts
+++ typescript_clean_architecture/src/interface/database/MySQL/UserRepositoryImpl.ts
@@ -1,7 +1,7 @@
 import { User } from "../../../domain/User";
 import { IUserRepository } from "../repository/user/IUserRepository";
 import { IDBConnection } from "./IDBConnection";
-import { toUpdateUserDTO, TCreateUserDTO } from "../repository/user/DTO";
+import { TCreateUserDTO, TUpdateUserDTO } from "../repository/user/DTO";
 
 class UserRepository extends IUserRepository {
     private connection: IDBConnection;
@@ -27,5 +27,37 @@ class UserRepository extends IUserRepository {
         return this.convertModel(queryResults[0]);
     }
 
+    public async findAll(): Promise<User[]> {
+       let queryResults = await this.connection.execute(
+        "select * from Users"
+       );
+       const results = queryResults.map(
+        (m): User => {
+            return this.convertModel(m);
+        }
+       );
+       return results;
+    }
 
+    public async create(createDTO: TCreateUserDTO): Promise<User> {
+        const user = await this.connection.execute(
+            'INSERT INTO Users (name, age) VALUES ("${createUserDTO.name}", "${createUserDTO.age}")'
+        );
+        return user;
+    }
+
+    public async update(userDTO: TUpdateUserDTO): Promise<User> {
+        const result = await this.connection.execute(
+            "update Users set name = ?, age = ?",
+            [userDTO.name, userDTO.age]
+        );
+        return result;
+    }
+
+    public async delete(id: number): Promise<null> {
+        await this.connection.execute("delete from Users where id = ?", id);
+        return null;
+    }
 }
+
+export { UserRepository };
--- typescript_clean_architecture/src/interface/database/repository/user/IUserRepository.ts
+++ typescript_clean_architecture/src/interface/database/repository/user/IUserRepository.ts
@@ -4,8 +4,8 @@ import { TUpdateUserDTO, TCreateUserDTO } from "./DTO";
 abstract class IUserRepository {
     abstract async findAll(): Promise<User[]>;
     abstract  async find(id: number): Promise<User>;
-    abstract async create(user: TCreateUserDTO): Promise<User>;
-    abstract async update(toUpdateUserDTO: TUpdateUserDTO): Promise<User>;
+    abstract async create(createDTO: TCreateUserDTO): Promise<User>;
+    abstract async update(userDTO: TUpdateUserDTO): Promise<User>;
     abstract async delete(id: number): Promise<null>;
 }
 
