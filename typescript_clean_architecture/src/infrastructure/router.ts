db41adac32da5f928d958b2ce423ad5e143a7250
Ukuma012 <Ukuma012@gmail.com>
2023-08-10 16:45:48 +0900

    serializer

--- typescript_clean_architecture/src/infrastructure/router.ts
+++ typescript_clean_architecture/src/infrastructure/router.ts
@@ -1,7 +1,54 @@
-import { Express } from "express";
+import express from "express";
 import Controller from "../interface/controller";
 import { TCreateUserRequest } from "../interface/request/user/CreateUserRequest";
 import { TFindUserRequest } from "../interface/request/user/FindUserRequest";
 import { UpdateUserRequest } from "../interface/request/user/UpdateUserRequest";
 import { TDeleteUserRequest } from "../interface/request/user/DeleteUserRequest";
 import { MysqlConnection } from "../infrastructure/MysqlConnection";
+
+const con = new MysqlConnection();
+const userController = new Controller.userController(con);
+const router = express.Router();
+
+// user
+router.get(
+    "/users",
+    async (_, res: express.Response): Promise<void> => {
+        let result = await userController.findAllUser();
+        res.send(result);
+    }
+);
+
+router.get(
+    "/users/:id",
+    async(req: TFindUserRequest, res: express.Response): Promise<void> => {
+        let result = await userController.findUser(req); 
+        res.send(result);
+    }
+);
+
+router.post(
+    "/users",
+    async (req: TCreateUserRequest, res: express.Response): Promise<void> => {
+        let result = await userController.CreateUser(req);
+        res.send(result);
+    }
+);
+
+router.patch(
+    "/users/:id",
+    async (req: UpdateUserRequest, res: express.Response): Promise<void> => {
+        let result = await userController.updateUser(req);
+        res.send(result);
+    }
+);
+
+router.delete(
+    "/users/:id",
+    async (req: TDeleteUserRequest, res: express.Response): Promise<void> => {
+        let result = await userController.deleteUser(req);
+        res.send(result);
+    }
+);
+
+export default router;
---
+++ typescript_clean_architecture/src/interface/controller/UserController.ts
@@ -0,0 +1,1 @@
+import { CreateUserRequest, TCreateUserRequest } from "../request/user/CreateUserRequest";
