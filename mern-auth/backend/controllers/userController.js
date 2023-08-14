ae0337149b1a6478d49d1e29917559437d91e81f
Ukuma012 <Ukuma012@gmail.com>
2023-08-14 13:40:54 +0900

    modified

--- mern-auth/backend/controllers/userController.js
+++ mern-auth/backend/controllers/userController.js
@@ -3,7 +3,7 @@ import generateToken from "../utils/generateToken.js";
 import User from '../models/userModel.js';
 
 // @desc    Auth user/set token
-// route    POST .api/users/auth
+// route    POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
