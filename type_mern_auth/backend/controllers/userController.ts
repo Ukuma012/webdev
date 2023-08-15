31e150914f327db9ef8311dd46cf04318680c24e
Ukuma012 <Ukuma012@gmail.com>
2023-08-15 15:39:10 +0900

    api

--- type_mern_auth/backend/controllers/userController.ts
+++ type_mern_auth/backend/controllers/userController.ts
@@ -5,9 +5,51 @@ import { Response, Request } from "express";
 // route    POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req: Request, res: Response) => {
-    res.json({
-        message: "Ok"
-    })
+  res.status(200).json({
+    message: "Ok",
+  });
 });
 
-export {authUser};
+// @desc    Register a new user
+// route    POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "User successfully registerd",
+  });
+});
+
+// @desc    Logout user
+// route    POST /api/users/logout
+// @access  Public
+const logoutUser = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "successfully logged out",
+  });
+});
+
+// @desc    Get user profile
+// route    GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Here is a user profile",
+  });
+});
+
+// @desc    Update user profile
+// route    POST /api/users/profile
+// @access  Private
+const updateuserProfile = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "successfully updated",
+  });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateuserProfile,
+};
--- type_mern_auth/backend/routes/userRoutes.ts
+++ type_mern_auth/backend/routes/userRoutes.ts
@@ -1,7 +1,17 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import { authUser } from '../controllers/userController';
+import {
+  authUser,
+  getUserProfile,
+  logoutUser,
+  registerUser,
+  updateuserProfile,
+} from "../controllers/userController";
 
+router.post("/", registerUser);
 router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.get("/profile", getUserProfile);
+router.post("/profile", updateuserProfile);
 
 export default router;
