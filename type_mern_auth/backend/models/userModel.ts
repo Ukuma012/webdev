3191d80771b4f59850f51b2693a41ce849145d24
Ukuma012 <Ukuma012@gmail.com>
2023-08-15 20:26:18 +0900

    userModel

---
+++ type_mern_auth/backend/models/userModel.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from 'bcryptjs';
+
+interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const userSchema: Schema = new Schema<IUser> ({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+userSchema.pre<IUser>('save', async function(next) {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+});
+
+const UserModel = mongoose.model<IUser>("User", userSchema);
+
+export default UserModel;
