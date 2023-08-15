09cb0953106ec3c27e1666eb2ce6f3ab65770d0a
Ukuma012 <Ukuma012@gmail.com>
2023-08-15 13:57:46 +0900

    nodemon concurrently

--- type_mern_auth/backend/server.ts
+++ type_mern_auth/backend/server.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
-	res.send('Server is ready');
+	res.send('Server is ready for TypeScript');
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
