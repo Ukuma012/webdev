bce24095c62313c93adf7faeaaf4fa00987b2ed6
Ukuma012 <Ukuma012@gmail.com>
2023-08-17 14:30:53 +0900

    redux

--- mern-auth/frontend/src/main.jsx
+++ mern-auth/frontend/src/main.jsx
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import store from "./store.js";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
@@ -15,16 +17,18 @@ import LoginScreen from "./screens/LoginScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={ <App /> }>
-      <Route index={true} path="/" element={ <HomeScreen /> } />
-      <Route path="/login" element={ <LoginScreen /> } />
-      <Route path="/register" element={ <RegisterScreen /> } />
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
-)
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
