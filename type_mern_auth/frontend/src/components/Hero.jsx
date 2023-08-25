6809efd48fc41630fff037a9e533694fcf46a373
Ukuma012 <Ukuma012@gmail.com>
2023-08-25 17:32:32 +0900

    members only

--- type_mern_auth/frontend/src/components/Hero.jsx
+++ type_mern_auth/frontend/src/components/Hero.jsx
@@ -1,24 +1,39 @@
 import { Container, Card, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { useSelector } from "react-redux";
 
 const Hero = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+
   return (
     <div className="py-5">
       <Container className="d-flex justify-content-center">
         <Card className="p-5 d-flex flex-column align-items-center hero-card bg-light w-75">
-          <h1 className="text-center mb-4">テストサイト</h1>
+          {userInfo ? (
+            <>
+              <h1 className="text-center mb-4">会員限定情報です！</h1>
+            </>
+          ) : (
+            <>
+              <h1 className="text-center mb-4">まだ見れません</h1>
+            </>
+          )}
           <p className="text-center mb-4">
-            会員限定の情報を見たいならアカウントを開設しないといけない
+            {userInfo ? (
+              <>ようこそ</>
+            ) : (
+              <>会員限定の情報を見たいならアカウントを開設しないといけない</>
+            )}
           </p>
           <div className="d-flex">
             <LinkContainer to="/login">
               <Button variant="primary" className="me-3">
-                サインイン 
+                サインイン
               </Button>
             </LinkContainer>
             <LinkContainer to="/register">
               <Button variant="secondary" href="/register" className="me-3">
-                いますぐ始める 
+                いますぐ始める
               </Button>
             </LinkContainer>
           </div>
