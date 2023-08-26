3edc4fa8c42eb95d992836883321470d7432b981
Ukuma012 <Ukuma012@gmail.com>
2023-08-26 16:11:52 +0900

    need to create mail server

--- nodemailer_demo/index.js
+++ nodemailer_demo/index.js
@@ -11,8 +11,38 @@ app.use(express.urlencoded({ extended: false }));
 const route = express.Router();
 const port = process.env.PORT || 5000;
 
-app.use('/', (req, res) => {
-    res.send('Server is ready');
-})
+
+app.use('/v1', route);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+const transporter = nodemailer.createTransport({
+    port: 25,
+    host: localhost,
+});
+
+route.post('/test-mail', (req, res) => {
+    const mailData = {
+        from: 'Fromアドレス',
+        to: 'Toアドレス',
+        subject: 'nodemailer test mail',
+        text: 'This is a test mail',
+    };
+
+    try {
+    transporter.sendMail(mailData, (error, info) => {
+        if(error) {
+            console.log("send failed");
+            console.log(error.message);
+            return;
+        }
+
+        console.log("send successful");
+        console.log(info.messageId);
+        res.status(200).json({ message: "send successful" });
+    });
+
+    } catch(e) {
+        console.log("Error", e);
+    }
+});
